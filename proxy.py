be6742017f4837f1ce452e22ebce814ca95af6d7
Matevz Vucnik <vucnikm@gmail.com>
2018-11-29 10:29:42 +0100

    fix

--- proxy.py
+++ proxy.py
@@ -1,36 +1,44 @@
 #!/usr/bin/env python3
 
 import json
-import asyncio
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
+from multiprocessing import Process, Queue
 
 def on_connect(mqttc, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     mqttc.subscribe("pmc/+")
 
 def on_message(mqttc, userdata, msg):
-    data = json.loads(msg.payload.decode('utf-8'))
-    node_id = data["node_id"]
-    ts = data["ts"]
-    del data["node_id"]
-    del data["ts"]
+    q.put_nowait(msg)
 
-    for field in data:
-        data[field] = float(data[field])
+def fwd_data(q):
+    influxdb = InfluxDBClient('localhost', 8086, 'pmc', 'secret', 'pmc')
 
-    influxdb_msg = [{"measurement": "pmc_data"}]
-    influxdb_msg[0]["timestamp"] = ts
-    influxdb_msg[0]["tags"] = {"host": node_id}
-    influxdb_msg[0]["fields"] = data
+    while True:
+        msg = q.get()
+        data = json.loads(msg.payload.decode('utf-8'))
+        node_id = data["node_id"]
+        ts = data["ts"]
+        del data["node_id"]
+        del data["ts"]
 
-    asyncio.create_task(influxdb.write_points(influxdb_msg))
+        for field in data:
+            data[field] = float(data[field])
+
+        influxdb_msg = [{"measurement": "pmc_data"}]
+        influxdb_msg[0]["timestamp"] = ts
+        influxdb_msg[0]["tags"] = {"host": node_id}
+        influxdb_msg[0]["fields"] = data
+
+        influxdb.write_points(influxdb_msg)
+
+q = Queue()
+Process(target=fwd_data, args=(q,)).start()
 
 mqttc = mqtt.Client()
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 mqttc.connect("localhost")
-
-influxdb = InfluxDBClient('localhost', 8086, 'pmc', 'secret', 'pmc')
 
 mqttc.loop_forever()
