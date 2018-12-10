63d1c3dd766bf33cae79c42e79cdb8c63a58717f
Matevz Vucnik <vucnikm@gmail.com>
2018-12-10 17:28:59 +0100

    fix

--- proxy.py
+++ proxy.py
@@ -2,17 +2,21 @@
 
 import os
 import json
+import queue
 import signal
+import multiprocessing
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
-from multiprocessing import Process, Queue
 
 def on_connect(mqttc, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     mqttc.subscribe("pmc/+")
 
 def on_message(mqttc, userdata, msg):
-    q.put_nowait(msg.payload)
+    try:
+        q.put_nowait(msg.payload)
+    except queue.Full:
+        os.kill(pid, signal.SIGTERM)
 
 def fwd_data(q, pid):
     influxdb = InfluxDBClient('localhost', 8086, 'pmc', 'secret', 'pmc')
@@ -41,8 +45,8 @@ def fwd_data(q, pid):
 
 pid = os.getpid()
 
-q = Queue()
-p = Process(target=fwd_data, args=(q, pid,))
+q = multiprocessing.Queue(100000)
+p = multiprocessing.Process(target=fwd_data, args=(q, pid,))
 p.daemon == True
 p.start()
 
