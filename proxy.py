fab0c3aef8497595a6a66a4d88612d6fd686091f
Matevz Vucnik <vucnikm@gmail.com>
2018-12-11 12:07:56 +0100

    fix

--- proxy.py
+++ proxy.py
@@ -22,22 +22,22 @@ def fwd_data(q, pid):
     influxdb = InfluxDBClient('localhost', 8086, 'pmc', 'secret', 'pmc')
 
     while True:
-        msg = q.get()
-        data = json.loads(msg.decode('utf-8'))
-        node_id = data["node_id"]
-        ts = data["ts"]
-        del data["node_id"]
-        del data["ts"]
-
-        for field in data:
-            data[field] = float(data[field])
-
-        influxdb_msg = [{"measurement": "pmc_data"}]
-        influxdb_msg[0]["timestamp"] = ts
-        influxdb_msg[0]["tags"] = {"host": node_id}
-        influxdb_msg[0]["fields"] = data
-
         try:
+            msg = q.get()
+            data = json.loads(msg.decode('utf-8'))
+            node_id = data["node_id"]
+            ts = data["ts"]
+            del data["node_id"]
+            del data["ts"]
+
+            for field in data:
+                data[field] = float(data[field])
+
+            influxdb_msg = [{"measurement": "pmc_data"}]
+            influxdb_msg[0]["timestamp"] = ts
+            influxdb_msg[0]["tags"] = {"host": node_id}
+            influxdb_msg[0]["fields"] = data
+
             influxdb.write_points(influxdb_msg)
         except:
             print("InfluxDB client error, restarting ...")
@@ -45,7 +45,7 @@ def fwd_data(q, pid):
 
 pid = os.getpid()
 
-q = multiprocessing.Queue(100000)
+q = multiprocessing.Queue(10000)
 p = multiprocessing.Process(target=fwd_data, args=(q, pid,))
 p.daemon == True
 p.start()
